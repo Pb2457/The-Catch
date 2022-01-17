0943d97041c5343293107960a730d777e68040f9
Pb2457 <81387445+Pb2457@users.noreply.github.com>
2022-01-16 20:22:07 -0500

    Add files via upload

---
+++ maindirect.js
@@ -0,0 +1,9 @@
+function nprNews(){
+    window.open('https://npr.org/')
+}
+function cnnNews(){
+    window.open('https://www.cnn.com/')
+}
+function nbcNews(){
+    window.open('https://www.nbcnews.com/us-news')
+}
